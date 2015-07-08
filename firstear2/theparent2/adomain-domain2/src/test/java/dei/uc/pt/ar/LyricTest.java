3d3283a4fe2aa57f0844888c52776946ebb2eb59
joaopedromartins <joao.pedro.brandao.martins@gmail.com>
2015-07-08 23:30:55 +0100

    Correccoes Pos entrega
    Correccoes Filipe Amaral

--- firstear2/theparent2/adomain-domain2/src/test/java/dei/uc/pt/ar/LyricTest.java
+++ firstear2/theparent2/adomain-domain2/src/test/java/dei/uc/pt/ar/LyricTest.java
@@ -1,7 +1,11 @@
 package dei.uc.pt.ar;
 
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertEquals;
 
+import org.hamcrest.Matcher;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
 import org.junit.Test;
 
 public class LyricTest {
@@ -13,6 +17,6 @@ public class LyricTest {
 		int expected = 0;
 		int result = l.getIdLyric();
 		assertEquals(expected, result);
-
+		//MatcherAssert.assertThat(result, is(expected));
 	}
 }
